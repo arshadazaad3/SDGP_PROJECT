e17fda7c89ca207847a2ef2d0a80d3487f44cd49
Fazna <47083276+DoryFazna@users.noreply.github.com>
2020-04-10 23:13:23 +0530

    Update TopTweetRecommender.py

--- Data_Science/TopTweetRecommender.py
+++ Data_Science/TopTweetRecommender.py
@@ -4,46 +4,48 @@ import pandas as pd
 
 # Step 1: Read CSV file
 df = pd.DataFrame(pd.read_excel('EnvironmentTweets.xlsx'))
+df_selected = df.iloc[2:]
 
 
-def get_index_from_hashtag(hashtag):
-    return df[df.hashtag == hashtag]["index"].values[0]
+# def get_index_from_tweet(tweet):
+#     return df[df.data == tweet]["index"].values[0]
 
-def get_hashtag_from_index(index):
-    return df[df.index == index]["hashtag"].values[0]
+def get_tweet_from_index(index):
+    return df_selected[df_selected.index == index]["data"].values[0]
 
-# Step 3: Create a colum in DF which combines all selected features
-def combine_features(row):
-    return row['hashtag'] + " " + row['tweet']
 
-def getRecommendation(input_hashtag):
-
-    # Step 2: Select Features
-    features = ['hashtag', 'tweet']
-
-    df["combined_features"] = df.apply(combine_features, axis=1)
+def getRecommendation():
+    print(df_selected)
 
     # Step 4: Create count matrix from this new combined column
     cv = CountVectorizer()
-    count_matrix = cv.fit_transform(df["combined_features"])
+
+    count_matrix = cv.fit_transform(df_selected["data"])
 
     # Step 5: Compute cosine similarity based on count_matrix
     cosine_sim = cosine_similarity(count_matrix)
 
-    hashtag_user_likes = input_hashtag
+    print(cosine_sim)
+    search_phrase_index = 0
 
-    # Step 6: Get Index of this movie from its title
-    hashtag_index = get_index_from_hashtag(hashtag_user_likes)
+    similar_tweets = list(enumerate(cosine_sim[search_phrase_index]))
 
-    similar_hashtags = list(enumerate(cosine_sim[hashtag_index]))
+    # Step 7: Get a list of similar tweets in descending order of similarity score
+    sorted_similar_tweets= sorted(similar_tweets, key=lambda x: x[1], reverse=True)
 
-    # Step 7: Get a list of similar movies in descending order of similarity score
-    sorted_similar_hashtags = sorted(similar_hashtags, key=lambda x: x[1], reverse=True)
+    print(sorted_similar_tweets)
 
-    # Step 8: Print titles of first 50 movies
-    for tweet in sorted_similar_hashtags:
-        print(get_hashtag_from_index(tweet[0]))
+    for tweet in sorted_similar_tweets:
+        print(get_tweet_from_index(tweet[0]+2))
 
+    toptweet = sorted_similar_tweets[1][0]
+    return toptweet
 
-getRecommendation("#saveAustralia")
+def runTopTweetRecommender():
+    toptweet = getRecommendation()
+    record = [(df.iloc[1][1], get_tweet_from_index(toptweet + 2))]
+    data_to_append = pd.DataFrame(record)
+    data_to_append.to_csv("faz.csv", mode='a', header=False)
+
+runTopTweetRecommender()
 
