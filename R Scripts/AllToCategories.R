01234b9a08b5cc8befdc203d874a1a0291616c9f
AhsanIlyas1998 <ahsan.2017190@iit.ac.lk>
2020-04-12 00:28:53 +0530

    First Commit, Added working folders for python and R

---
+++ R Scripts/AllToCategories.R
@@ -0,0 +1,9 @@
+##########################
+# DATASET CREATION
+
+# We will be using R scripts to create a dataset with the following information
+#       - Name of the Article
+#       - Content of the Article
+#       - Category of the Article
+
+
