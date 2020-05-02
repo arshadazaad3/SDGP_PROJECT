bc08f4f84999cf948ad2e07d67187d6c2d680906
AhsanIlyas1998 <ahsan.2017190@iit.ac.lk>
2020-05-02 23:59:05 +0530

    test.Model.py => where the pickel file is beung tested

---
+++ Data_Science/Text Classifier/testModel.py
@@ -0,0 +1,40 @@
+import os
+import pickle
+
+import sklearn.datasets as skd
+
+# news_test = skd.load_files('E:\Studies\Give A Try\SDGP\Data Set\\Test BBC', encoding='ISO-8859-1')
+news_test = skd.load_files('Test BBC', encoding='ISO-8859-1')
+# print(news_test['target_names'])
+listOfTopics = news_test['target_names']
+
+# Importing the trained model
+with open('text_classifier_model', 'rb') as training_model:
+    model = pickle.load(training_model)
+
+final_result = []
+try:
+    # x = (model.predict(news_test.data))[0]
+    x = model.predict(news_test.data)
+    # print(x)
+    for i in x:
+        final_result.append(listOfTopics[i])
+        # print(listOfTopics[i])
+    # print(listOfTopics[x])
+except:
+    print("SOME ERROR OCCURRED")
+
+# print(final_result)
+# Finding the maximum occur in the list
+try:
+    final_result_achieved = max(final_result, key=final_result.count)
+except:
+    final_result_achieved = "SOME ERROR OCCURRED"
+print(final_result_achieved)
+
+# After the final result is stored the .txt files are removed
+dir_name = ('Test BBC/business')
+test = os.listdir(dir_name)
+for item in test:
+    if item.endswith(".txt"):
+        os.remove(os.path.join(dir_name, item))
