f12dabe65d213cbe4793c6aa8b27b54a7d1c30c5
Fazna <47083276+DoryFazna@users.noreply.github.com>
2020-05-04 12:38:15 +0530

    Update TopHashtagRecommender.py

--- Data_Science/TopHashtagRecommender.py
+++ Data_Science/TopHashtagRecommender.py
@@ -1,74 +1,62 @@
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import pandas as pd
-import pymongo
 
+#get the index of the given hashtag
+def get_index_from_hashtag(hashtag,dataframe):
+    return dataframe[dataframe.hashtag == hashtag]["index"].values[0]
 
-def get_index_from_hashtag(hashtag):
-    return df[df.hashtag == hashtag]["index"].values[0]
+#get hastag of the given index
+def get_hashtag_from_index(index,dataframe):
+    return dataframe[dataframe.index == index]["hashtag"].values[0]
 
-def get_hashtag_from_index(index):
-    return df[df.index == index]["hashtag"].values[0]
-
-# Step 3: Create a colum in DF which combines all selected features
+# Create a column in DF which combines all target phrases
 def combine_features(row):
     return row['hashtag'] + " " + row['tweet']
 
-def getRecommendation(input_hashtag):
+def getTopHashtag(dataframe, target_hashtag):
 
-    # Step 2: Select Features
-    features = ['hashtag', 'tweet']
+    dataframe["combined_features"] = dataframe.apply(combine_features, axis=1)
 
-    df["combined_features"] = df.apply(combine_features, axis=1)
-
-    pd.set_option('display.max_columns', None)
-    print(df)
-
-
-    # Step 4: Create count matrix from this new combined column
+    # Create count matrix from this new combined column
     cv = CountVectorizer()
+    count_matrix = cv.fit_transform(dataframe["combined_features"])
 
-    count_matrix = cv.fit_transform(df["combined_features"])
-
-
-    # Step 5: Compute cosine similarity based on count_matrix
+    # Compute cosine similarity based on count_matrix
     cosine_sim = cosine_similarity(count_matrix)
 
-    print(cosine_sim)
+    # Get Index of user Entered phrase
+    hashtag_index = get_index_from_hashtag(target_hashtag,dataframe)
 
-    hashtag_user_likes = input_hashtag
-
-    # Step 6: Get Index of this hashtag from its title
-    hashtag_index = get_index_from_hashtag(hashtag_user_likes)
-
+    # get similar ratio matrix
     similar_hashtags = list(enumerate(cosine_sim[hashtag_index]))
 
-    # Step 7: Get a list of similar movies in descending order of similarity score
+    # Get a list of similar hastags in descending order of similarity score
     sorted_similar_hashtags = sorted(similar_hashtags, key=lambda x: x[1], reverse=True)
 
-    print(sorted_similar_hashtags)
-    # Step 8: Print titles of first 50 movies
+    # store top 5 hastags in an array
+    results = []
+    if(len(sorted_similar_hashtags) >6):
+        num = 6
+    else:
+        num = len(sorted_similar_hashtags)
+    for i in range(0,num-1):
+        tweet = sorted_similar_hashtags[i+1]
+        results.append(get_hashtag_from_index(tweet[0],dataframe))
+    return results
 
-    for tweet in sorted_similar_hashtags:
-        print(get_hashtag_from_index(tweet[0]))
-
-
-
-def runTopHashtagRecommender(hashtag,category):
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["SDGP"]
-    mycol = mydb[category]
-
-    global df
+# TOP HASHTAG RECOMMENDER MAIN CODE
+def runTopHashtag(category, target_Hashtag):
+    #access database to get top tweets of the target category
+    mydb = client[category]
+    mycol = mydb["TopTweet"]
     df = mycol.find()
     df = pd.DataFrame(df)
-    print(df)
 
-    getRecommendation(hashtag)
+    top5Hashtags =getTopHashtag(df,target_Hashtag) # get top 5 hashtags
 
+    #store top 5 hastags in the database
+    mycol = client[target_Hashtag]
+    col = mycol["topfivehashtags"]
+    for x in range(1, len(top5Hashtags)+1):
+        result = {"index": x, "tweet": top5Hashtags[x - 1]}
+        col.insert_one(result)
 
-
-runTopHashtagRecommender("SaveAustralia","Environment")
-
-
-
+runTopHashtag(category,keywordToSearch)
